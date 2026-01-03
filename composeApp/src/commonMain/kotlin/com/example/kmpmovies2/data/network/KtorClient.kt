666ebca3dc743b7c99818312c3f2d927e0ec6d3b
Matheus Angelicio <matheus.angelicio00@gmail.com>
2026-01-03 09:30:03 -0300

    refactor(network): update Ktor logging to use SIMPLE logger

--- composeApp/src/commonMain/kotlin/com/example/kmpmovies2/data/network/KtorClient.kt
+++ composeApp/src/commonMain/kotlin/com/example/kmpmovies2/data/network/KtorClient.kt
@@ -13,6 +13,7 @@ import io.ktor.client.plugins.logging.DEFAULT
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.plugins.logging.SIMPLE
 import io.ktor.client.request.get
 import io.ktor.client.request.parameter
 import io.ktor.http.HttpHeaders
@@ -45,7 +46,7 @@ class KtorClient {
             }
         }
         install(Logging) {
-            logger = Logger.DEFAULT
+            logger = Logger.SIMPLE
             level = LogLevel.ALL
             sanitizeHeader { header -> header == HttpHeaders.Authorization }
         }
